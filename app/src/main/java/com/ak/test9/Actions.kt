682ee6ebfe3852dfaa50a090247215a4b0e044e2
ak <lampphone092@gmail.com>
2024-05-27 21:07:50 +0530

    Xposed Updated-2(NATIVE METHOD SUCCESS)

--- app/src/main/java/com/ak/test9/Actions.kt
+++ app/src/main/java/com/ak/test9/Actions.kt
@@ -1,5 +1,6 @@
 package com.ak.test9
 
+import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
@@ -84,7 +85,38 @@ class Actions: IXposedHookLoadPackage {
                     }
                 }
             )
-        }
-    }
 
-}
+            XposedHelpers.findAndHookMethod(
+                "com.ak.testx2.MainActivity",
+                lpparam.classLoader,
+                "onCreate",
+                Bundle::class.java,
+                object : XC_MethodHook() {
+                    @Throws(Throwable::class)
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        XposedBridge.log("Hooked into MainActivity2 onCreate")
+                        try {
+                            // Access the activity instance
+                            val activity = param.thisObject as Activity
+                            // Find the TextView by its ID
+                            val textView_N = activity.findViewById<TextView>(
+                                activity.resources.getIdentifier(
+                                    "textView_N", "id", activity.packageName))
+                            // Change the color of the TextView text
+                            textView_N.setTextColor(Color.BLACK)
+                            // Call the native method
+                            val nativeText = XposedHelpers.callMethod(activity, "native_Xposed") as String
+                            val additionalText = "\n***Xposed Framework***"
+                            textView_N.text = "$additionalText"
+
+                        } catch (e: Exception) {
+                            XposedBridge.log(e)
+                        }
+                    }
+                }
+            )
+
+        } // Package name equals
+    }// handleLoadPackage
+
+}// Actions
